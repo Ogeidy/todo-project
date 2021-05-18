22860ab1e2cd4fff6c5a148336d85034290aa451
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-18 19:02:16 +0300

    users: serializers: add 'id' field to UserModelSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "email", "first_name", "last_name"]
+        fields = ["id", "username", "email", "first_name", "last_name"]
