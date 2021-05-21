0dfe28b8cd70a00a7beabf2e82afabd20544b4d1
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-21 18:19:09 +0300

    frontend: add users components

---
+++ frontend/src/components/User.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+
+const UserItem = ({ user }) => {
+    return (
+        <tr>
+            <td>
+                {user.username}
+            </td>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({ users }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Username
+                    </th>
+                    <th>
+                        First name
+                    </th>
+                    <th>
+                        Last Name
+                    </th>
+                    <th>
+                        Email
+                    </th>
+                </tr>
+            </thead>
+            <tbody>{users.map((user) => <UserItem user={user} />)}</tbody>
+        </table>
+    )
+}
+
+
+export default UserList
