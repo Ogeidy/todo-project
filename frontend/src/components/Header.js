dbad5a209616f133dcdfde2c3b75495671231daf
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-22 11:58:12 +0300

    frontend: components: add Header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+import Menu from './Menu.js'
+
+const Header = () =>
+    <header className="header">
+        <div>TODO project</div>
+        <Menu />
+    </header>
+
+export default Header;
