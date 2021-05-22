bd0542b43b0f030532ae719a89cc28a7cff21551
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-22 11:57:25 +0300

    frontend: components: add Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () =>
+    <footer className="footer">
+        <p>TODO project <br/> Copyright &copy; 2021</p>
+    </footer>
+
+export default Footer;
