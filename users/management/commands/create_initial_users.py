eab237132cada9c6a30111449906ef560791a8fd
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-22 12:00:24 +0300

    users: add first and last names to creation command

--- users/management/commands/create_initial_users.py
+++ users/management/commands/create_initial_users.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.core.management.base import BaseCommand
 
 from users.models import User
@@ -8,7 +11,27 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print("Creating...")
-        User.objects.create_superuser(username="admin", email="admin@example.com", password="test")
-        for i in range(2):
-            User.objects.create_user(username=f"test{i}", email=f"test{i}@example.com", password="test")
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="test",
+            first_name=generate_first_name(),
+            last_name=generate_last_name(),
+        )
+        for i in range(50):
+            User.objects.create_user(
+                username=f"test{i}",
+                email=f"test{i}@example.com",
+                password="test",
+                first_name=generate_first_name(),
+                last_name=generate_last_name(),
+            )
         print("Done!")
+
+
+def generate_first_name():
+    return "".join(random.choices(string.ascii_lowercase, k=5)).capitalize()
+
+
+def generate_last_name():
+    return "".join(random.choices(string.ascii_lowercase, k=10)).capitalize()
