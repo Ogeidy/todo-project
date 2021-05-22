9305d9d1db6444c9f63a2e9ac04465a6addd34f1
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-22 11:57:52 +0300

    frontend: components: add Menu

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Menu.css'
+
+const Menu = () =>
+    <nav>
+        <ul className="navbar_list">
+            <li><a className="navbar_link" href="#">Users</a></li>
+            <li><a className="navbar_link" href="#">Wip #1</a></li>
+            <li><a className="navbar_link" href="#">Wip #2</a></li>
+        </ul>
+    </nav>
+
+export default Menu;
