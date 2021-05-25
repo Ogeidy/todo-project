fd8550db8798e83c08e9de46ff1627b6715c3fce
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-25 23:07:34 +0300

    todo: urls: 'projects' and 'notes' urls

--- todo/urls.py
+++ todo/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserModelViewSet
+from projects.views import ProjectModelViewSet, NoteModelViewSet
 
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('notes', NoteModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
