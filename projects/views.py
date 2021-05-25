330b8f3bfa4e0117523e52403ed7e8a74d11c946
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-25 23:05:39 +0300

    projects: views: add viewsets

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from projects.models import Project, Note
+from projects.serializers import ProjectModelSerializer, NoteModelSerializer
 
-# Create your views here.
+
+class ProjectModelViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class NoteModelViewSet(ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteModelSerializer
