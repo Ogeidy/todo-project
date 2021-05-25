535c408e990e73c5cc30679727c3f5fe19e1e163
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-25 23:03:26 +0300

    projects: add 'project' and 'note' models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=200)
+    repository = models.URLField()
+    contributors = models.ManyToManyField(User)
+
+
+class Note(models.Model):
+    name = models.CharField(max_length=32)
+    text = models.TextField()
+    author = models.ForeignKey(User, models.PROTECT)
+    project = models.ForeignKey(Project, models.PROTECT)
+    creation_date = models.DateTimeField()
+    modification_date = models.DateTimeField()
+    active = models.BooleanField()
