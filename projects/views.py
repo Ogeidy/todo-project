0bfda0f904d58991e9d6a7774f62779b5cd303d2
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-28 23:07:28 +0300

    projects: views: add ProjectPagination

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
-from projects.models import Project, Note
-from projects.serializers import ProjectModelSerializer, NoteModelSerializer
+from rest_framework import pagination
+
+from projects import models, serializers
+
+
+class ProjectPagination(pagination.LimitOffsetPagination):
+    default_limit = 10
 
 
 class ProjectModelViewSet(ModelViewSet):
-    queryset = Project.objects.all()
-    serializer_class = ProjectModelSerializer
+    queryset = models.Project.objects.all()
+    serializer_class = serializers.ProjectModelSerializer
+    pagination_class = ProjectPagination
 
 
 class NoteModelViewSet(ModelViewSet):
-    queryset = Note.objects.all()
-    serializer_class = NoteModelSerializer
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteModelSerializer
