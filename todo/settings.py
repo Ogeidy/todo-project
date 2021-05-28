71b8b0278e83f01e5c0239a2f7fdc3548bc83b3a
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-28 23:25:10 +0300

    todo: settings: add django-filter

--- todo/settings.py
+++ todo/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
+    "django_filters",
     "users",
     "projects",
 ]
@@ -44,6 +45,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 CORS_ALLOWED_ORIGINS = [
