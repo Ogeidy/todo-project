1b33e676860c49ec8de26548f71d8d1c445fbc58
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-05-28 23:52:56 +0300

    projects: views: add pagination and filter for Note

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,4 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework import pagination
+from rest_framework import viewsets, pagination, response, status
 
 from projects import models, serializers, filters
 
@@ -8,13 +7,25 @@ class ProjectPagination(pagination.LimitOffsetPagination):
     default_limit = 10
 
 
-class ProjectModelViewSet(ModelViewSet):
+class NotePagination(pagination.LimitOffsetPagination):
+    default_limit = 20
+
+
+class ProjectModelViewSet(viewsets.ModelViewSet):
     queryset = models.Project.objects.all()
     serializer_class = serializers.ProjectModelSerializer
     pagination_class = ProjectPagination
     filterset_class = filters.ProjectFilter
 
 
-class NoteModelViewSet(ModelViewSet):
+class NoteModelViewSet(viewsets.ModelViewSet):
     queryset = models.Note.objects.all()
     serializer_class = serializers.NoteModelSerializer
+    pagination_class = NotePagination
+    filterset_class = filters.NoteFilter
+
+    def destroy(self, request, pk=None):
+        note = models.Note.objects.get(pk=pk)
+        note.active = False
+        note.save()
+        return response.Response(status=status.HTTP_200_OK)
