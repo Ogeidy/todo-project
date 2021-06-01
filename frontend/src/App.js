c19a3bed162f3480635c0651b807f2b134e05d92
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-06-02 00:34:05 +0300

    frontend: App: add Notes and Projects components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,72 @@
-import React from 'react'
-import axios from 'axios'
-import './App.css';
-import UserList from './components/User.js'
-import Footer from './components/Footer.js'
-import Header from './components/Header.js'
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import UserList from "./components/Users.js";
+import ProjectList from "./components/Projects.js";
+import NoteList from "./components/Notes.js";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            'users': []
-        }
+            users: [],
+            projects: [],
+            notes: [],
+        };
     }
 
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users')
-            .then(response => {
-                const users = response.data
-                this.setState(
-                    {
-                        'users': users
-                    }
-                )
-            }).catch(error => console.log(error))
+        axios
+            .get("http://127.0.0.1:8000/api/users/")
+            .then((response) => {
+                const users = response.data.results;
+                this.setState({
+                    users: users,
+                });
+            })
+            .catch((error) => console.log(error));
+        axios
+            .get("http://127.0.0.1:8000/api/projects/")
+            .then((response) => {
+                const projects = response.data.results;
+                this.setState({
+                    projects: projects,
+                });
+            })
+            .catch((error) => console.log(error));
+        axios
+            .get("http://127.0.0.1:8000/api/notes/")
+            .then((response) => {
+                const notes = response.data.results;
+                this.setState({
+                    notes: notes,
+                });
+            })
+            .catch((error) => console.log(error));
     }
 
     render() {
         return (
             <div className="App">
-                <Header />
-                <UserList users={this.state.users} />
-                <Footer />
+                <BrowserRouter>
+                    <Header />
+                    <Route exact path="/">
+                        <UserList users={this.state.users} />
+                    </Route>
+                    <Route exact path="/projects">
+                        <ProjectList projects={this.state.projects} users={this.state.users} />
+                    </Route>
+                    <Route exact path="/notes">
+                        <NoteList notes={this.state.notes} />
+                    </Route>
+                    <Footer />
+                </BrowserRouter>
             </div>
-        )
+        );
     }
-
 }
 
 export default App;
