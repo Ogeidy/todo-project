c4278293c85d1f200e5776c73284432eb080f8ce
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-06-02 01:40:11 +0300

    frontend: components: move from table to grid layout

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -5,41 +5,31 @@ import { Link } from "react-router-dom";
 
 const ProjectItem = ({ project, users }) => {
     return (
-        <tr>
-            <td>{project.id}</td>
-            <td>{project.name}</td>
-            <td>{project.description}</td>
-            <td>{project.repository}</td>
-            <td>
-                {project.contributors.length}
-            </td>
-        </tr>
+        <Link className="table_body table_row row_link" to={`project/${project.id}`}>
+            <div className="table_cell">{project.id}</div>
+            <div className="table_cell">{project.name}</div>
+            <div className="table_cell">{project.description}</div>
+            <div className="table_cell">{project.repository}</div>
+            <div className="table_cell">{project.contributors.length}</div>
+        </Link>
     );
 };
 
 const ProjectList = ({ projects, users }) => {
     return (
         <div className="list">
-            <table className="table">
-                <thead className="table_head">
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Repository</th>
-                        <th>Contributors</th>
-                    </tr>
-                </thead>
-                <tbody className="table_body">
-                    {projects.map((project) => (
-                        <ProjectItem
-                            project={project}
-                            users={users}
-                            key={project.id}
-                        />
-                    ))}
-                </tbody>
-            </table>
+            <div className="table">
+                <div className="table_head table_row">
+                    <div className="table_cell">Id</div>
+                    <div className="table_cell">Name</div>
+                    <div className="table_cell">Description</div>
+                    <div className="table_cell">Repository</div>
+                    <div className="table_cell">Contributors</div>
+                </div>
+                {projects.map((project) => (
+                    <ProjectItem project={project} users={users} key={project.id} />
+                ))}
+            </div>
         </div>
     );
 };
