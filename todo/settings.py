b0e95e823026910642e75fa01049f97a75c01d53
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-06-04 20:28:49 +0300

    todo: add JWT token authorization

--- todo/settings.py
+++ todo/settings.py
@@ -54,6 +54,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
 
