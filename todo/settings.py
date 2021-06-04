c21012b371c62fab09b57d26736e7891c942300d
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-06-04 19:44:42 +0300

    todo: add basic, session and token authorization

--- todo/settings.py
+++ todo/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "corsheaders",
     "django_filters",
     "users",
@@ -46,6 +47,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
 
 CORS_ALLOWED_ORIGINS = [
--- todo/urls.py
+++ todo/urls.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+
 from users.views import UserModelViewSet
 from projects.views import ProjectModelViewSet, NoteModelViewSet
 
 
 router = DefaultRouter()
-router.register('users', UserModelViewSet)
-router.register('projects', ProjectModelViewSet)
-router.register('notes', NoteModelViewSet)
+router.register("users", UserModelViewSet)
+router.register("projects", ProjectModelViewSet)
+router.register("notes", NoteModelViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include(router.urls)),
+    path("api-token-auth/", views.obtain_auth_token),
 ]
