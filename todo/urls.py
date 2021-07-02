dd7be0103f0021998b58d03dc2494f1e66d78c0a
Iakov Mamontov <iakov.mamontov@emlid.com>
2021-07-02 20:03:39 +0300

    todo: urls: add view for docs

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+import rest_framework
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -14,6 +18,18 @@ router.register(prefix=r"(?P<version>\d\.\d)/users", viewset=UserModelViewSet)
 router.register("projects", ProjectModelViewSet)
 router.register("notes", NoteModelViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library",
+        default_version="0.1",
+        description="Documentation to out project",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[rest_framework.permissions.AllowAny],
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
@@ -21,4 +37,7 @@ urlpatterns = [
     path("api/auth-token/", views.obtain_auth_token),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
